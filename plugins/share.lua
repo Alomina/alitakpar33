639be70f0496d28ab9158c02bca73da254c967fa
ДGЄИт☆☆☆☆☆FДz <holyscroll@users.noreply.github.com>
2016-05-06 18:38:16 +0000

    Update share.lua

--- plugins/share.lua
+++ plugins/share.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)     -- Bot number     -- first name        -- last name
-send_contact(get_receiver(msg), "+989160578290", "Teleking", "@", ok_cb, false)
+send_contact(get_receiver(msg), "+15732504368", "Teleking", "@antispam_king_team", ok_cb, false)
 end
 
 return {
