d5f8e54c36fa5c0248e3d5c81d7eae4c441f50b0
ДGЄИт☆☆☆☆☆FДz <holyscroll@users.noreply.github.com>
2016-05-06 16:51:53 +0430

    Update TMP_selfbot.lua

--- bot/TMP_selfbot.lua
+++ bot/TMP_selfbot.lua
@@ -225,7 +225,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {PutYourID(BOTID),tonumber(our_id)},--Sudo users
+    sudo_users = {85546349},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
